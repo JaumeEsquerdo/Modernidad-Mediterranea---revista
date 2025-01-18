9f0c44b57da3ed0e4d1834babc7fd4da88ebe84e
Jaume <jaume.esquerdo@alumnos.cei.es>
2025-01-18 18:16:48 +0100

    meter /

--- js/global.js
+++ js/global.js
@@ -4,7 +4,7 @@ const headerNavs = document.querySelectorAll('.Header-navItem');
 const currentURL = window.location.href;
 
 // detectar si estoy en github pages o en local
-const basePath = window.location.pathname.includes('/proyecto-final') ? '/proyecto-final' : '/';
+const basePath = window.location.pathname.includes('/proyecto-final') ? '/proyecto-final/' : '/';
 
 headerNavs.forEach(nav => {
     const href = nav.getAttribute('href'); // para obtener el href del .Header-navItem del html
